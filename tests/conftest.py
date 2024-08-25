d0e03c52c9bb527c70e9a445f8baa5075bdebb17
Joe <joeinnz@hotmail.com>
2024-08-25 22:50:56 +1200

    update pytest code

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from app import app
 
 @pytest.fixture
 def client():
-    app = create_app()  # Create an instance of webapp
+    app = app()  # Create an instance of webapp
     app.config['TESTING'] = True  # Enable testing mode
     with app.test_client() as client:
         yield client
