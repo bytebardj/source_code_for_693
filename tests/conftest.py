df398bf99ebce87b3dbe736874b0d36259479747
Joe <joeinnz@hotmail.com>
2024-08-27 20:33:59 +1200

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
-from app import app
+from app import views
 
 @pytest.fixture
 def client():
-    with app.test_client() as client:
+    with views.test_client() as client:
         yield client
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,6 +1,3 @@
-import pytest
-
-@pytest.mark.skipif(not connect.dbuser, reason="No database connection available")
 def test_homepage(client):
     response = client.get('/')
     assert response.status_code == 200
