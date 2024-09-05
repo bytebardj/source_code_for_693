5ab098e5995335b626e72cfb5d2fd7bb3bf26c7f
Joe <joeinnz@hotmail.com>
2024-09-05 21:50:28 +1200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
