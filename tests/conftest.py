41ab639f67670526acd48be3c9fe48deeb107de4
Joe <joeinnz@hotmail.com>
2024-09-08 08:22:49 +1200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import coverage
+import os
 
 cov = coverage.Coverage(source=['app'])  # Replace 'app' with your main application package
 cov.start()
