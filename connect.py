978ee01727f22c96bd26385cdfd6f601a176ca4f
Joe <joeinnz@hotmail.com>
2024-09-15 08:18:02 +1200

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "root" #PUT YOUR MySQL username here - usually root
-dbpass = "Joever090816" #PUT YOUR PASSWORD HERE
-dbhost = "localhost" 
+dbuser = "joehunt" #PUT YOUR MySQL username here - usually root
+dbpass = "J.oever090816" #PUT YOUR PASSWORD HERE
+dbhost = "devops693.mysql.database.azure.com" 
 dbport = "3306"
 dbname = "fresh_harvest_delivery_db"
