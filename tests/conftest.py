21b2402d4334c787d35c88bba3f96ef746d12431
Joe <joeinnz@hotmail.com>
2024-09-22 16:30:01 +1200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -94,3 +94,4 @@ def test_invalid_route(client):
     """Test that an invalid route returns a 404 error"""
     response = client.get('/nonexistent-page')
     assert response.status_code == 404
+    
